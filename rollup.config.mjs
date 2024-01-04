62fa5e1399a28eea294f7bcda199295f564fc5c4
DY414 <tmvldghkfur@gmail.com>
2024-01-04 16:28:43 +0900

    - test

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -13,8 +13,28 @@ export default [
             typescript({ tsconfig: './tsconfig.json' }),
             postcss({
                 inject: true,
-                extensions: ['.css', '.scss'],
-            })
+                minimize: true,
+                extensions: ['.css', '.scss']
+            }),
+            {
+                buildEnd() {
+                  const fs = require('fs');
+          
+                  function getFileSizeInBytes(filename) {
+                    const stats = fs.statSync(filename);
+                    const fileSizeInBytes = stats.size;
+                    return fileSizeInBytes;
+                  }
+          
+                  const sizeBefore = getFileSizeInBytes('path/to/css/file.css');
+                  //... minimize 적용 ...
+                  const sizeAfter = getFileSizeInBytes('path/to/css/file.css');
+          
+                  console.log(`Before: ${sizeBefore} bytes`);
+                  console.log(`After: ${sizeAfter} bytes`);
+                  console.log(`Difference: ${sizeBefore - sizeAfter} bytes`);
+                }
+              }
         ]
     },
     {
